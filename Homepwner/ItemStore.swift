49521ac4ff8658dd6c6a8853c4f2957298676062
Gabriel <gabriel1786@gmail.com>
2016-02-04 17:06:10 -0200

    Respostas do Capítulo 9 Bronze, Silver e Gold Challenges

--- Homepwner/ItemStore.swift
+++ Homepwner/ItemStore.swift
@@ -11,12 +11,23 @@ import UIKit
 class ItemStore {
     
     var allItems = [Item]()
+    // Bronze Challenge
+    var valueMoreThan50 = [Item]()
+    var valueLessThan50 = [Item]()
+    //
     
     func createItem() -> Item {
         let newItem = Item(random: true)
         
         allItems.append(newItem)
         
+        // Bronze Challenge
+        if (newItem.valueInDollars > 50){
+            valueMoreThan50.append(newItem)
+        } else {
+            valueLessThan50.append(newItem)
+        }
+        //
         return newItem
     }
     
--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -25,6 +25,12 @@ class ItemsViewController: UITableViewController {
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
+        if (section == 0) {
+            return itemStore.valueMoreThan50.count
+        }
+        if (section == 1) {
+            return itemStore.valueLessThan50.count + 1 //Silver Challenge
+        }
         return itemStore.allItems.count
     }
     
@@ -36,12 +42,74 @@ class ItemsViewController: UITableViewController {
         // Get a new or recycled cell
         let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
         
-        // Set the text on the cell with the description of the item that is at the nth index of items, where n = row this cell will appear in on the tableview
-        let item = itemStore.allItems[indexPath.row]
-        
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        // Bronze Challenge
+        if (indexPath.section == 0){
+            let item = itemStore.valueMoreThan50[indexPath.row]
+            
+            cell.textLabel?.font = UIFont(name: "Avenir", size: 16)
+            cell.textLabel?.text = item.name
+            cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+            
+            return cell
+        }
+        if (indexPath.section == 1) {
+            if (indexPath.row < itemStore.valueLessThan50.count) { //Silver Challenge
+                let item = itemStore.valueLessThan50[indexPath.row]
+                
+                cell.textLabel?.font = UIFont(name: "Avenir", size: 16)
+                cell.textLabel?.text = item.name
+                cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+                
+                return cell
+                
+            } else {
+                cell.textLabel?.text = "NO MORE ITEMS!"
+                cell.textLabel?.font = UIFont(name: "Avenir", size: 12)
+                cell.textLabel?.textColor = UIColor.whiteColor()
+                cell.detailTextLabel?.text = ""
+                cell.backgroundColor? = UIColor.orangeColor()
+            }
+        }
+//        
+//        // Set the text on the cell with the description of the item that is at the nth index of items, where n = row this cell will appear in on the tableview
+//        let item = itemStore.allItems[indexPath.row]
+//        
+//        cell.textLabel?.text = item.name
+//        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
         
         return cell
+        }
+
+    // Bronze Challenge
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2;
     }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch(section){
+            case 0: return "Value More Than 50"
+            case 1: return "Value Less Than 50"
+            default: return ""
+        }
+    }
+    // Gold Challenge
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if (indexPath.section == 0) {
+            return 60
+        }
+        if (indexPath.section == 1) {
+            if (indexPath.row < itemStore.valueLessThan50.count) {
+                return 60
+            } else {
+                return 20
+            }
+        }
+        return 44
+    }
+    
+    
+    
+    
+    
+    
 }
