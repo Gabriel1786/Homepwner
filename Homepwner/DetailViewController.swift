f59069198a17ed5de1f73695e7a441ff6a51df32
Gabriel <gabriel1786@gmail.com>
2016-03-15 16:12:05 -0300

    Fim do capitulo 14

--- Homepwner/DetailViewController.swift
+++ Homepwner/DetailViewController.swift
@@ -8,21 +8,39 @@
 
 import UIKit
 
-class DetailViewController: UIViewController, UITextFieldDelegate {
+class DetailViewController: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     @IBOutlet var nameField: UITextField!
     @IBOutlet var serialNumberField: UITextField!
     @IBOutlet var valueField: UITextField!
     @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var imageView: UIImageView!
     
     @IBAction func backgroundTapped(sender: UITapGestureRecognizer) {
         view.endEditing(true)
     }
+    @IBAction func takePicture(sender: UIBarButtonItem) {
+        
+        let imagePicker = UIImagePickerController()
+        
+        // If the device has a camera, take a picture; otherwise, just pick from photo library
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            imagePicker.sourceType = .Camera
+        } else {
+            imagePicker.sourceType = .PhotoLibrary
+        }
+        imagePicker.delegate = self
+        
+        // Place image picker on the screen
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
     var item: Item! {
         didSet {
             navigationItem.title = item.name
         }
     }
+    var imageStore: ImageStore!
     
     let numberFormatter: NSNumberFormatter = {
         let formatter = NSNumberFormatter()
@@ -42,15 +60,20 @@ class DetailViewController: UIViewController, UITextFieldDelegate {
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Clear First responder
-        view.endEditing(true)
+//        // Clear First responder
+//        view.endEditing(true)
         
         nameField.text = item.name
         serialNumberField.text = item.serialNumber
         valueField.text = numberFormatter.stringFromNumber(item.valueInDollars)
         dateLabel.text = dateFormatter.stringFromDate(item.dateCreated)
         
+        // Get the item key
+        let key = item.itemKey
         
+        // If there is an associated image with the item display it on the image view
+        let imageToDisplay = imageStore.imageForKey(key)
+        imageView.image = imageToDisplay
     }
     
     override func viewWillDisappear(animated: Bool) {
@@ -71,5 +94,20 @@ class DetailViewController: UIViewController, UITextFieldDelegate {
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        // Get picked image from info dictionary
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        // Store the image in the ImageStore for the item's key
+        imageStore.setImage(image, forKey: item.itemKey)
+        
+        // Put that image on the screen in the image view
+        imageView.image = image
+        
+        // Take image picker off the screen - you must call this dismiss method
+        dismissViewControllerAnimated(true, completion: nil)
     }
 }
---
+++ Homepwner/ImageStore.swift
@@ -0,0 +1,26 @@
+//
+//  ImageStore.swift
+//  Homepwner
+//
+//  Created by Graduando on 3/15/16.
+//  Copyright © 2016 DP Content. All rights reserved.
+//
+
+import UIKit
+
+class ImageStore {
+    
+    let cache = NSCache()
+    
+    func setImage(image: UIImage, forKey key: String) {
+        cache.setObject(image, forKey: key)
+    }
+    
+    func imageForKey(key: String) -> UIImage? {
+        return cache.objectForKey(key) as? UIImage
+    }
+    
+    func deleteImageForKey(key: String) {
+        cache.removeObjectForKey(key)
+    }
+}
